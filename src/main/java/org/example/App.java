8359cde4e9a4cb686f280ed71fb7fb6cb352d35d
Nuraiym Mamatova <mnurajym9@gmail.com>
2023-01-02 17:09:35 +0600

    app class clean

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,11 +2,10 @@ package org.example;
 
 /**
  * Hello Nuraiym!
- *
+ * Hello Eldiar!
  */
-public class App 
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
