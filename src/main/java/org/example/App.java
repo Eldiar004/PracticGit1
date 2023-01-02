bb91e06f7064e09d56afd1c5a2d5663bfb1a9b74
Eldiar004 <eldiarro2004@gmail.com>
2023-01-02 16:57:48 +0600

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Hello Nuraiym!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
